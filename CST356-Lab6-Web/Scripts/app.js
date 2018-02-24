e4a846e72f7d73309fe9091202c692da81c25742
ZedTR <zed.trzcinski@gmail.com>
2018-02-24 01:43:25 -0800

    init

---
+++ CST356-Lab6-Web/Scripts/app.js
@@ -0,0 +1,18 @@
+﻿function CollectData() {
+    
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var age =  document.getElementById("age").value;
+    var years = document.getElementById("years").value;
+
+    if (fname == "" || lname == "" || age == "") {
+      document.getElementById("unSReg").innerText = "unSuccessful Registeration";
+    }
+    else {
+        document.getElementById("Pfname").innerText = fname;
+        document.getElementById("Plname").innerText = lname;
+        document.getElementById("Page").innerText = age;
+        document.getElementById("Pyears").innerText = years;
+}
+    } 
+ 

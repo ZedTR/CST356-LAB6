efdfa4d833aa82b14cf7f60657d479a970af6766
ZedTR <zed.trzcinski@gmail.com>
2018-02-24 02:17:40 -0800

    updates

--- CST356-Lab6-Web/Scripts/app.js
+++ CST356-Lab6-Web/Scripts/app.js
@@ -3,7 +3,7 @@
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
     var age =  document.getElementById("age").value;
-    var years = document.getElementById("years").value;
+  
 
     if (fname == "" || lname == "" || age == "") {
       document.getElementById("unSReg").innerText = "unSuccessful Registeration";
@@ -12,7 +12,7 @@
         document.getElementById("Pfname").innerText = fname;
         document.getElementById("Plname").innerText = lname;
         document.getElementById("Page").innerText = age;
-        document.getElementById("Pyears").innerText = years;
+      
 }
     } 
  
